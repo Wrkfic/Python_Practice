8e9c2b632b67f9d5c34d3ba9e50103940e08c0e2
Anuj Shukla <85061651+Wrkfic@users.noreply.github.com>
2021-06-09 11:14:21 +0530

    removing whitespaces
    This code will help to remove all whitespaces between any given sentence.

---
+++ CountAlpha.py
@@ -0,0 +1,9 @@
+#code will count alphabets, commas, special character, etc. In other words it removes whitespaces between your given sentebces.
+def lettercount(string):
+    l=string.split(" ")
+    length=0
+    for i in l:
+        length+=len(i)
+    return length
+sentence=input("please enter your sentence:")
+print(lettercount(sentence))
